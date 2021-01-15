b15df9c51f71087437408b32212dd4f2fecc3f4f
Cvaniak <igna.cwaniak@gmail.com>
2021-01-15 23:37:22 +0100

    Its Math Bot now

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # DsBotHW
 Hello World with Discord Bot
 First steps with [freecodecamp](https://www.freecodecamp.org/news/create-a-discord-bot-with-python/)  
+Math eval based on [jfs StackOverflow](https://stackoverflow.com/a/9558001/11425694)  
 ---
 To start install ```pip install -r requirements.txt```  
 Then add file ```.env``` with line ```TOKEN="BOT TOKEN FROM DISCORD WEBSITE"```  
--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 import discord
 import os
 from dotenv import load_dotenv
+import ast
+import operator as op
 load_dotenv()
+
+# supported operators
+operators = {ast.Add: op.add, ast.Sub: op.sub, ast.Mult: op.mul,
+             ast.Div: op.truediv, ast.Pow: op.pow, ast.BitXor: op.xor,
+             ast.USub: op.neg}
+
+def eval_expr(expr):
+    """
+    >>> eval_expr('2^6')
+    4
+    >>> eval_expr('2**6')
+    64
+    >>> eval_expr('1 + 2*3**(4^5) / (6 + -7)')
+    -5.0
+    """
+    return eval_(ast.parse(expr, mode='eval').body)
+
+def eval_(node):
+    if isinstance(node, ast.Num): # <number>
+        return node.n
+    elif isinstance(node, ast.BinOp): # <left> <operator> <right>
+        return operators[type(node.op)](eval_(node.left), eval_(node.right))
+    elif isinstance(node, ast.UnaryOp): # <operator> <operand> e.g., -1
+        return operators[type(node.op)](eval_(node.operand))
+    else:
+        raise TypeError(node)
 
 client = discord.Client()
 
@@ -14,7 +42,22 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+    
+    if message.content.startswith("$math "):
+        try:
+            result = eval_expr(message.content[6:])
+            await message.channel.send(result)
+        except:
+            await message.channel.send("I'm just simple bot but it's ain't math equations")
+            
+    #For simple math only to not test everything
+    if len(message.content) < 16:
+        try:
+            result = eval_expr(message.content)
+            await message.channel.send(result)
+        except:
+            pass
 
 client.run(os.getenv('TOKEN'))
