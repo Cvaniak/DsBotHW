1ade3db2b72d0e5d45673286f9cd6f07a7b6cd7f
Cvaniak <igna.cwaniak@gmail.com>
2021-01-16 03:52:48 +0100

    Files for heroku deploy, $ counter for every curse word

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # DsBotHW
+Bot can say ```Hello!``` on ```$hello```  
+It can do math for you! Just write some short equation and it will answer! For longer equations start with ```$math```.  
+Also it will count dollar from every user that will curse!  
+---
 Hello World with Discord Bot
-First steps with [freecodecamp](https://www.freecodecamp.org/news/create-a-discord-bot-with-python/)  
+First steps done with [freecodecamp](https://www.freecodecamp.org/news/create-a-discord-bot-with-python/)  
 Math eval based on [jfs StackOverflow](https://stackoverflow.com/a/9558001/11425694)  
 ---
-To start install ```pip install -r requirements.txt```  
+First install ```pip install -r requirements.txt```  
 Then add file ```.env``` with line ```TOKEN="BOT TOKEN FROM DISCORD WEBSITE"```  
 Then run with ```python3 main.py```
--- main.py
+++ main.py
@@ -3,8 +3,18 @@ import os
 from dotenv import load_dotenv
 import ast
 import operator as op
+import requests
+import json
+from tinydb import TinyDB, Query
+import atexit
 load_dotenv()
 
+# Delete files with rude words when program is off
+@atexit.register
+def goodbye():
+    os.remove("profanity.txt")
+    os.remove("wulgaryzmy.json")
+    
 # supported operators
 operators = {ast.Add: op.add, ast.Sub: op.sub, ast.Mult: op.mul,
              ast.Div: op.truediv, ast.Pow: op.pow, ast.BitXor: op.xor,
@@ -50,8 +60,20 @@ async def on_message(message):
             result = eval_expr(message.content[6:])
             await message.channel.send(result)
         except:
-            await message.channel.send("I'm just simple bot but it's ain't math equations")
-            
+            await message.channel.send("No ziomek, to nie jest wyrażenie matematyczne...")
+    
+    # Bad words test
+    word_list = message.content.split(" ")
+    if any(word in full_list for word in word_list):
+        db_query = db.search(DSUser.name == message.author.name)
+        if db_query:
+            count_t = int(db_query[0]["count"])+1
+            db.update({"count": count_t}, DSUser.name == message.author.name)
+            await message.channel.send(F"Oj, oj już wisisz nam {message.author.name} całe {count_t}$!")
+        else:
+            db.insert({"name": message.author.name, "count": 1})
+            await message.channel.send(F"No wiesz {message.author.name}, za takie słowa naliczam Ci dolca!")
+
     #For simple math only to not test everything
     if len(message.content) < 16:
         try:
@@ -59,5 +81,22 @@ async def on_message(message):
             await message.channel.send(result)
         except:
             pass
+
+if not os.path.isfile('wulgaryzmy.json'):
+    r = requests.get("https://raw.githubusercontent.com/coldner/wulgaryzmy/master/wulgaryzmy.json")
+    open('wulgaryzmy.json', "wb").write(r.content)
+with open('wulgaryzmy.json') as wul_file:
+    wul_list = json.load(wul_file)
+
+if not os.path.isfile('profanity.txt'):
+    r = requests.get("https://raw.githubusercontent.com/RobertJGabriel/Google-profanity-words/master/list.txt")
+    open('profanity.txt', "wb").write(r.content)
+with open("profanity.txt") as prof_file:
+    prof_list = prof_file.read().splitlines()
+
+full_list = prof_list + wul_list
+
+db = TinyDB('db.json')
+DSUser = Query()
 
 client.run(os.getenv('TOKEN'))
